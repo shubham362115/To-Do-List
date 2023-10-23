1e7d5a3329157852200bcf3e1c307fbcefd9a2a1
Shubham Shukla <125434549+shubham362115@users.noreply.github.com>
2023-10-23 07:02:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let input = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add(){
+    if (input.value == ""){
+        alert("Please Enter Task")
+    }else{
+        let newEle = document.createElement("ul");
+        newEle.innerHTML = `${input.value} <i class="fa-solid fa-trash" ></i>`;
+        text.appendChild(newEle);
+
+        input.value = "";
+
+        newEle.querySelector("i").addEventListener("click" , remove)
+
+        function remove(){
+            newEle.remove()
+        }
+    }
+}
